c17daaf873a5c4c953a2c425ffb3ac4a259c8c15
srsujon37 <srsujon97@gmail.com>
2025-03-02 14:59:51 +0600

    Date Update

---
+++ script/date.js
@@ -0,0 +1,8 @@
+const d = new Date().toLocaleDateString('en-GB', {
+    weekday: 'short',
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+});
+
+document.getElementById("date").innerHTML = d;
